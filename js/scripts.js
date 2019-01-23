fbe2cad0d2ee3e96e2ad7078ab8f83e79145acba
Joe Wheelock <joemwheelock@gmail.com>
2019-01-23 14:03:34 -0800

    set up basic js logic

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,20 @@ let colors = [
     "rgb(0, 255, 255)"
 ]
 
-let squares = document.querySelectorAll("square");
+let squares = document.querySelectorAll(".square");
+let pickedColor = colors[3];
+let colorDisplay = document.querySelector("colorDisplay");
 
-for(i = 0; i < squares.length; i++) {
-    squares[i].style.backgroundColor = colors[i];
+for (i = 0; i < squares.length; i++) {
+    //add initial colors to squares
+    squares[i].style.backgroundColor= colors[i];
+    //add click listeners to squares
+    squares[i].addEventListener("click", function(){
+        let clickedColor = this.style.backgroundColor;
+        if(clickedColor === pickedColor){
+            alert("Correct!");
+        } else {
+            alert("Wrong!");
+        }
+    })
 }
