2bd87854e77dc5ca50ba0cae299d16f1364353cd
Joe Wheelock <joemwheelock@gmail.com>
2019-01-27 12:29:29 -0800

    reset func, styles, comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
-let colors = [
-    "rgb(255, 0, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 255, 0)",
-    "rgb(255, 0, 255)",
-    "rgb(0, 255, 255)"
-]
+let colors = generateRandomColors(6);
 
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.querySelector("#colorDisplay");
 let messageDisplay = document.querySelector("#message");
+let h1 = document.querySelector("h1");
+let resetButton = document.querySelector("#reset");
 
+//Clicking Reset Button
+resetButton.addEventListener("click", function() {
+    colors = generateRandomColors(6);
+    pickedColor = pickColor();
+    resetButton.textContent = "New Colors";
+    colorDisplay.textContent = pickedColor;
+    for(i = 0; i < squares.length; i++){
+        squares[i].style.background = colors[i];
+    }
+    h1.style.backgroundColor = "#232323";
+})
+
+//Display RGB to guess
 colorDisplay.textContent = pickedColor;
 
 for (i = 0; i < squares.length; i++) {
@@ -22,7 +30,9 @@ for (i = 0; i < squares.length; i++) {
         let clickedColor = this.style.backgroundColor;
         if(clickedColor === pickedColor){
             messageDisplay.textContent = "Correct!";
+            resetButton.textContent = "Play Again?";
             changeColors(pickedColor);
+            h1.style.backgroundColor = clickedColor;
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again";
@@ -30,12 +40,28 @@ for (i = 0; i < squares.length; i++) {
     })
 }
 
+//Functions
 function changeColors(color) {
     for(i = 0; i< squares.length; i++)
-    squares[i].style.background = color;
+    squares[i].style.backgroundColor = color;
 }
 
 function pickColor() {
     let random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+function generateRandomColors(num) {
+    let arr = [];
+    for(i = 0; i < num; i++) {
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
